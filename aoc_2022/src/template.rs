b2ac3c4bfccf14fc0c3fc29ea7f056e5be8d5277
Alexander Diaz <diaz.alexander@vast-inc.com>
2022-12-09 09:39:27 -0500

    improved template

--- aoc_2022/src/template.rs
+++ aoc_2022/src/template.rs
@@ -1,11 +1,9 @@
-use std::{ffi::OsString, fs::read_to_string};
+use std::{fs::read_to_string, path::Path};
 
 #[allow(dead_code)]
-pub fn day00(input_path: &OsString) -> (String, String) {
+pub fn day00(input_path: &Path) -> (String, String) {
     let input: String = read_to_string(input_path).expect("Error reading file");
-    for line in input.split('\n') {
-        let mut _tokens = line.split_whitespace();
-    }
+    for _ in input.split('\n') {}
     let p1: usize = 0;
     let p2: usize = 0;
     (p1.to_string(), p2.to_string())
